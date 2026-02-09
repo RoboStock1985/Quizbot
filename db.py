c2eaa4548a8ddbdf968c07fc5b29b4919533e114
Dave Shaw <davidshaw1985@gmail.com>
2026-02-09 12:54:29 +0000

    Updated secrets

--- db.py
+++ db.py
@@ -1,8 +1,5 @@
 import os
 from supabase import create_client, Client
-from dotenv import load_dotenv
-
-load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
