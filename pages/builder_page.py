c7fc8de0d1843a7828db8d2ed678f68f8f27fb15
Dave Shaw <davidshaw1985@gmail.com>
2026-02-09 16:46:33 +0000

    changed to add src_bytes

--- pages/builder_page.py
+++ pages/builder_page.py
@@ -202,6 +202,11 @@ def build_builder_page(page, categories, topics, selected_categories, selected_t
         pdf.output(temp_path)
 
         file_picker = ft.FilePicker()
+
+        # add in src_bytes to make this work in web environments where we can't write to temp files
+        with open(temp_path, "rb") as f:
+            file_picker.src_bytes = f.read()
+            
         save_path = await file_picker.save_file(file_name="quiz_export.pdf", allowed_extensions=["pdf"])
 
         if save_path:
